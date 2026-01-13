0135cd28d635ffc1c41846ab15993ba1bc29c9e7
keshavsoft <nknnkumar@yahoo.com>
2026-01-13 19:38:04 +0530

    only chat kept

--- app/src/main/java/com/example/compose/jetchat/feature/voicetotext/VoiceModels.kt
+++
@@ -1,7 +0,0 @@
-package com.example.compose.jetchat.feature.voicetotext
-
-data class VoiceBubble(
-    val id: Long = System.currentTimeMillis(),
-    val text: String,
-    val isMe: Boolean = true
-)
